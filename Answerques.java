d836805204a5d6226641bb7336b658b13265d7a6
hartej-kathuria <hartej.kathuria@gmail.com>
2016-09-21 00:39:57 +0530

    answering a qurstion in three try

---
+++ Answerques.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+class Answerques
+{
+	public static void main(String[] args)
+	{
+		String answer;
+		String correctanswer="six";
+		int i=0;
+		Scanner userip=new Scanner(System.in);
+		while(i!=3)
+		{
+			System.out.print("what is the value obtained by multypling  two with three(answer in words):"+" ");
+			answer=userip.next();
+			if (answer.equals(correctanswer))
+			{
+				System.out.println("Good");
+				break;
+			}
+
+			else
+				if (i!=2)
+				{
+					System.out.println("Incorrect answer, try again");
+					i++;
+				}	  
+		}
+		if (i==3)
+			System.out.println("Incorrect answer,he correct answer="+correctanswer);
+	}
+}

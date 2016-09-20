c58cdf1dea2b4c57b68177ba3175454dfffc8393
hartej-kathuria <hartej.kathuria@gmail.com>
2016-09-20 18:06:13 +0530

    number divisble by 7 betwwen 100 and 200

---
+++ Divby7.java
@@ -0,0 +1,17 @@
+class Divby7
+{
+	public static void main(String[] args)
+	{
+		int i,j,k;
+		int a[]=new int[100];
+		for(i=100;i<=200;i++)
+		{
+			if (i%7==0)
+			{
+				System.out.print(i + ",");  
+			}
+
+		}
+		System.out.println();
+	}
+}
